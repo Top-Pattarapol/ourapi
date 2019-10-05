9af6d1fcfb066b98b6e56674bcbe0112b8eef749
Pattarapol Sawasdee <pattarapol.s@kbtg.tech>
2019-10-05 09:50:08 +0700

    add struct user and mock data

--- main.go
+++ main.go
@@ -6,11 +6,22 @@ import (
 	"net/http"
 )
 
+type Users struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Phone    string `json:"phone"`
+}
+
 func main()  {
+	users := []Users{
+		{1,"Leanne Graham","Bret", "Sincere@april.biz", "1-770-736-8031 x56442"},
+	}
 
 	e := echo.New()
 	e.GET("/users", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, map[string]string{"hello": "hi"})
+		return c.JSON(http.StatusOK, users)
 	})
 	log.Fatal(e.Start(":1234"))
 }
