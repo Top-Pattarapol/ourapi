187666e4f5a152ed5351983dd978be041c67de42
Pattarapol Sawasdee <pattarapol.s@kbtg.tech>
2019-10-05 09:41:11 +0700

    set serview

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/labstack/echo"
+	"log"
+	"net/http"
+)
 
 func main()  {
-	fmt.Println("hello, kub")
+
+	e := echo.New()
+	e.GET("/users", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{"hello": "hi"})
+	})
+	log.Fatal(e.Start(":1234"))
 }
